ca1a22206475733ea8996b445ac0879bc80ecaf8
lhwon <lhwon8767@i-gns.co.kr>
2023-05-03 10:08:43 +0900

    오류수정

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,17 +1,23 @@
-
+//import '@mdi/font/css/materialdesignicons.css' // material design icons
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+//import colors from 'vuetify/lib/util/colors'
 
+// import '@/assets/theme/vuetify/scss/ai.scss' // Custom
+// import '@/assets/theme/vuetify/scss/tagview.scss' // tagview
+//import theme from '@/assets/theme/vuetify/js/aiColorTable' // color table
+import '@/plugins/vuetify/scss/khnp.scss'
+// import ko from '@/i18n/vuetify/ko' // vuetify i18n
+// import en from '@/i18n/vuetify/en' // vuetify i18n
 
-
-import 'D:/Front/my-front-end/src/plugins/vuetify/scss/khnp.scss' // Custom
-
-
-
-
-
-
-
+// const opts = {
+//   theme: theme.theme,
+//   icons: theme.icons,
+//   lang: {
+//     locales: { ko, en },
+//     current: 'ko'
+//   }
+// }
 
 Vue.use(Vuetify)
 
